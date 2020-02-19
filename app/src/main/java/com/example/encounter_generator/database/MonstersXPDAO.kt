ef95ee3b7a7e584498e5ce7e27b02cc47bba670f
gguyver <glen.guyverfletcher@gmail.com>
2020-02-19 15:30:24 +0000

    Added three main Activites, worked layout
    Layout still needs working on, and the code which interacts with buttons and database etc.

--- app/src/main/java/com/example/encounter_generator/database/MonstersXPDAO.kt
+++ app/src/main/java/com/example/encounter_generator/database/MonstersXPDAO.kt
@@ -1,8 +1,10 @@
 package com.example.encounter_generator.database
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface MonstersXPDAO {
-    // TODO: add logic before the queries?
+    @Query("SELECT monster_name FROM monster_xp WHERE experience <= :XP")
+    fun getMonstersUnderXPAmount(XP : Int) : List<String>
 }
--- app/src/main/java/com/example/encounter_generator/database/MultipliersDAO.kt
+++ app/src/main/java/com/example/encounter_generator/database/MultipliersDAO.kt
@@ -1,8 +1,21 @@
 package com.example.encounter_generator.database
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface MultipliersDAO {
     // need to implement party-size multiplier here as well, using lead/lag SQL function
+
+    // Use for parties 3-5 members
+    @Query("SELECT encounter_multiplier FROM encounter_multipliers WHERE :encounterSize >= minimum_encounter_size AND :encounterSize <= maximum_encounter_size")
+    fun getNormalMultiplier(encounterSize : Int) : Int
+
+    // Use for parties <3 members
+    @Query("SELECT LAG(encounter_multiplier) FROM encounter_multipliers WHERE :encounterSize >= minimum_encounter_size AND :encounterSize <= maximum_encounter_size")
+    fun getSmallPartyMultiplier(encounterSize : Int) : Int
+
+    // Use for parties >5 members
+    @Query("SELECT LEAD(encounter_multiplier) FROM encounter_multipliers WHERE :encounterSize >= minimum_encounter_size AND :encounterSize <= maximum_encounter_size")
+    fun getLargePartyMultiplier(encounterSize : Int) : Int
 }
