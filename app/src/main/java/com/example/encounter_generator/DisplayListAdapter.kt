0b008e05739fa31914994cf563a4f87b0bd2452e
gguyver <glen.guyverfletcher@gmail.com>
2020-02-21 13:59:14 +0000

    Works!
    Had to rework entity because it was reading it as Real even though
    sqlite explorer said the column was integer. Also had to rework
    RecyclerView adapter so it actually filled in the values instead of
    crashing.

--- app/src/main/java/com/example/encounter_generator/DisplayListAdapter.kt
+++ app/src/main/java/com/example/encounter_generator/DisplayListAdapter.kt
@@ -1,26 +1,35 @@
 package com.example.encounter_generator
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_encounter_parameters.view.*
 
 class DisplayListAdapter (private val data : List<String>) : RecyclerView.Adapter<DisplayListAdapter.MyViewHolder>() {
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         // create new view
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_row, parent, false) as TextView
-        return MyViewHolder(textView)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_row, parent, false)
+        return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.textView.text = data[position]
+        holder.bindText(data[position])
     }
 
     override fun getItemCount() = data.size
+
+    class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        private var txtName = view.findViewById<TextView>(R.id.monster_name_view)
+        fun bindText(text : String) {
+            txtName.text = text
+        }
+    }
 }
